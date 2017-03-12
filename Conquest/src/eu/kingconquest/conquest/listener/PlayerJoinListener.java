b31464e6cea93a9e30b09f9238ec1ccd90989688
Liquid <thomas.lundqvist.1991@gmail.com>
2017-03-12 22:10:16 +0100

    Version 0.7.4.3
    Permissions problem

--- Conquest/src/eu/kingconquest/conquest/listener/PlayerJoinListener.java
+++ Conquest/src/eu/kingconquest/conquest/listener/PlayerJoinListener.java
@@ -6,11 +6,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import eu.kingconquest.conquest.Main;
 import eu.kingconquest.conquest.Scoreboard.KingdomBoard;
 import eu.kingconquest.conquest.Scoreboard.NeutralBoard;
 import eu.kingconquest.conquest.Scoreboard.PlayerBoard;
 import eu.kingconquest.conquest.core.PlayerWrapper;
 import eu.kingconquest.conquest.database.YmlStorage;
+import eu.kingconquest.conquest.hook.Vault;
+import eu.kingconquest.conquest.util.Validate;
 
 public class PlayerJoinListener implements Listener{
 	private static PlayerWrapper wrapper;
@@ -24,7 +27,8 @@ public class PlayerJoinListener implements Listener{
 	public void onPlayerJoin(PlayerJoinEvent e){
 		Player player = e.getPlayer();
 		YmlStorage.getWorlds().forEach(uniqueID->{
-			
+			if (!Validate.hasPerm(player, ".basic"))
+				Vault.perms.playerAdd(player,  Main.getInstance().getName() + ".basic");
 			if (player.getWorld().equals(Bukkit.getWorld(uniqueID))){
 				wrapper = PlayerWrapper.getWrapper(player);
 				if (!wrapper.isInKingdom(Bukkit.getWorld(uniqueID))){
